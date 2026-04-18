37bac9110bffc056a6c2a2e69d77c026a4229c4e
Anthony <chinemerem.aguzie283@gmail.com>
2026-04-18 16:54:43 -0700

    Update SQL analysis

--- analysis.sql
+++ analysis.sql
@@ -1,3 +1,39 @@
-SELECT sex, Avg(survived) AS survival_rate
+-- Titanic Data Analysis
+
+-- Survival by Gender
+SELECT sex, AVG(survived) AS survival_rate
 FROM titanic
 GROUP BY sex;
+
+-- Survival by Class
+SELECT pclass, AVG(survived) AS survival_rate
+FROM titanic
+GROUP BY pclass;
+
+-- Survival by Age Group
+SELECT 
+  CASE 
+    WHEN age < 18 THEN 'Child'
+    ELSE 'Adult'
+  END AS age_group,
+  AVG(survived) AS survival_rate
+FROM titanic
+GROUP BY age_group;
+
+-- Survival by Family Size
+SELECT 
+  CASE 
+    WHEN sibsp + parch + 1 = 1 THEN 'Alone'
+    ELSE 'With Family'
+  END AS family_type,
+  AVG(survived) AS survival_rate
+FROM titanic
+GROUP BY family_type;
+
+-- Top Titles by Survival
+SELECT 
+  SUBSTR(name, INSTR(name, ',') + 2, INSTR(name, '.') - INSTR(name, ',') - 2) AS title,
+  AVG(survived) AS survival_rate
+FROM titanic
+GROUP BY title
+ORDER BY survival_rate DESC;
